211626374dbc5e423cdf6f048f391632d6ffa8cf
Stek40 <39171474+Stek40@users.noreply.github.com>
2021-09-15 16:16:33 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -9,7 +9,7 @@ class Board
 public:
     Board(int height, int width);
     void print() const;
-    void update(int x, int y);
+    void update(int);
     bool win() const;
 private:
     char board[MAX_HEIGHT][MAX_WIDTH];
