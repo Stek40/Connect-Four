57a4a7c400d463aabc521afdd1ec5dda26998ccb
Stek40 <39171474+Stek40@users.noreply.github.com>
2021-09-16 15:53:02 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+
+int main()
+{
+    Board b = Board(10,15);
+
+    b.update(1);
+    b.print();
+    b.update(1);
+    b.print();
+    b.update(2);
+    b.print();
+    b.update(2);
+    b.print();
+    b.update(3);
+    b.print();
+    b.update(3);
+    b.print();
+    b.update(4);
+    if (b.win()) 
+    {
+        int a;
+        std::cin>>a;
+    };
+    b.print();
+    b.update(2);
+    b.print();
+
+
+    return 0;
+}
