d91a1de4da21a90c6cafdc455b7ad9723364f3c2
Stek40 <39171474+Stek40@users.noreply.github.com>
2021-09-19 21:41:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
+#include "Game.h"
 
 
 int main()
 {
-    Board b = Board(10,15);
+    Game game1 = Game(10, 15);
 
-    b.update(1);
-    b.print();
-    b.update(1);
-    b.print();
-    b.update(2);
-    b.print();
-    b.update(2);
-    b.print();
-    b.update(3);
-    b.print();
-    b.update(3);
-    b.print();
-    b.update(4);
-    if (b.win()) 
-    {
-        int a;
-        std::cin>>a; //here it must stop because it is vertical win 
-    };
-    b.print();
-    b.update(2);
-    b.print();
+    game1.play();
+
 
 
     return 0;
